485b1c3b5d469668209e5a33bec05158c640dd3f
p7m <perpetuum7@protonmail.com>
2022-07-15 10:18:20 +0200

    feat: support polygon

--- src/constants.ts
+++ src/constants.ts
@@ -10,6 +10,7 @@ export class AddressByNetwork {
     public boba_mainnet: string;
     public aurora_mainnet: string;
     public moonriver_mainnet: string;
+    public polygon_mainnet: string;
     public dev: string;
 }
 
@@ -47,6 +48,8 @@ function forNetwork(
         return Address.fromString(addressByNetwork.aurora_mainnet);
     } else if (network == "moonriver") {
         return Address.fromString(addressByNetwork.moonriver_mainnet);
+    } else if (network == "matic") {
+        return Address.fromString(addressByNetwork.polygon_mainnet);
     } else {
         return Address.fromString(addressByNetwork.dev);
     }
@@ -56,6 +59,7 @@ const vaultAddressByNetwork: AddressByNetwork = {
     boba_mainnet: "0x2A4409Cc7d2AE7ca1E3D915337D1B6Ba2350D6a3",
     aurora_mainnet: "0x0613ADbD846CB73E65aA474b785F52697af04c0b",
     moonriver_mainnet: "0xEa1E627c12DF4e054D61FD408Ff7186353aC6cA1",
+    polygon_mainnet: "0xACf8489ccb47DA2D7306d827bbEDe05bFA6fea1b",
     dev: "0x0000000000000000000000000000000000000000"
 };
 
@@ -63,6 +67,7 @@ const wethAddressByNetwork: AddressByNetwork = {
     boba_mainnet: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000",
     aurora_mainnet: "0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB",
     moonriver_mainnet: "0x98878B06940aE243284CA214f92Bb71a2b032B8A",
+    polygon_mainnet: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
     dev: "0x0000000000000000000000000000000000000000"
 };
 
@@ -70,6 +75,7 @@ const kyoAddressByNetwork: AddressByNetwork = {
     boba_mainnet: "0x618CC6549ddf12de637d46CDDadaFC0C2951131C",
     aurora_mainnet: "0x0000000000000000000000000000000000000000",
     moonriver_mainnet: "0x0000000000000000000000000000000000000000",
+    polygon_mainnet: "0x0000000000000000000000000000000000000000",
     dev: "0x0000000000000000000000000000000000000000"
 };
 
@@ -77,6 +83,7 @@ const usdAddressByNetwork: AddressByNetwork = {
     boba_mainnet: "0x7562F525106F5d54E891e005867Bf489B5988CD9", // FRAX
     aurora_mainnet: "0xB12BFcA5A55806AaF64E99521918A4bf0fC40802", // USDC
     moonriver_mainnet: "0x1A93B23281CC1CDE4C4741353F3064709A16197d", // FRAX
+    polygon_mainnet: "0x45c32fA6DF82ead1e2EF74d17b76547EDdFaFF89", // FRAX
     dev: "0x0000000000000000000000000000000000000000"
 };
 
@@ -84,6 +91,7 @@ const fraxAddressByNetwork: AddressByNetwork = {
     boba_mainnet: "0x7562F525106F5d54E891e005867Bf489B5988CD9",
     aurora_mainnet: "0xE4B9e004389d91e4134a28F19BD833cBA1d994B6",
     moonriver_mainnet: "0x1A93B23281CC1CDE4C4741353F3064709A16197d",
+    polygon_mainnet: "0x45c32fA6DF82ead1e2EF74d17b76547EDdFaFF89",
     dev: "0x0000000000000000000000000000000000000000"
 };
 
@@ -91,6 +99,7 @@ const daiAddressByNetwork: AddressByNetwork = {
     boba_mainnet: "0xf74195Bb8a5cf652411867c5C2C5b8C2a402be35",
     aurora_mainnet: "0xe3520349F477A5F6EB06107066048508498A291b",
     moonriver_mainnet: "0x80A16016cC4A2E6a2CACA8a4a498b1699fF0f844",
+    polygon_mainnet: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
     dev: "0x0000000000000000000000000000000000000000"
 };
 
@@ -98,6 +107,7 @@ const usdcAddressByNetwork: AddressByNetwork = {
     boba_mainnet: "0x66a2A913e447d6b4BF33EFbec43aAeF87890FBbc",
     aurora_mainnet: "0xB12BFcA5A55806AaF64E99521918A4bf0fC40802",
     moonriver_mainnet: "0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D",
+    polygon_mainnet: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
     dev: "0x0000000000000000000000000000000000000000"
 };
 
@@ -105,6 +115,7 @@ const usdtAddressByNetwork: AddressByNetwork = {
     boba_mainnet: "0x5DE1677344D3Cb0D7D465c10b72A8f60699C062d",
     aurora_mainnet: "0x4988a896b1227218e4A686fdE5EabdcAbd91571f",
     moonriver_mainnet: "0xFFFFFFfFea09FB06d082fd1275CD48b191cbCD1d",
+    polygon_mainnet: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
     dev: "0x0000000000000000000000000000000000000000"
 };
 
